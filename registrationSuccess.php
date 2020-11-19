099226cf417517047805e09c1bad4b537dff203d
k1ngk0sm0 <k1ngk0sm0@protonmail.com>
2020-11-19 10:52:12 -0500

    meets all registration page project requirements

--- registrationSuccess.php
+++ registrationSuccess.php
@@ -1,3 +1,39 @@
-<?php
-echo "success";
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="CSS/styles.css">
+    <title>The Freshness</title>
+  </head>
+  <body>
+      
+    <!-- Navbar and Logo -->
+    <?php require_once "navbar.php"; ?>
+
+    <!-- Main Content -->
+    <br><br>
+    <div class="card text-center">
+        <div class="card-body">
+            <h5 class="card-title">Thanks for signing up!</h5>
+            <p class="card-text">You now have access to the freshest local meal kits.</p>
+            <a href="#" class="btn btn-success">Log In</a>
+        </div>
+    </div>
+    <br><br>
+
+
+    <!-- Bootstrap Javascript-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+
+  <!-- Footer -->
+  <?php require_once "footer.php"; ?>
+  
+</html>
