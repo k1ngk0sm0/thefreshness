e9bc76cd09825b9134232563e617a02fb48a14e6
k1ngk0sm0 <k1ngk0sm0@protonmail.com>
2020-12-04 14:29:17 -0500

    add logout functionality

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+
+// Logout user
+session_start();
+$_SESSION = array();
+setcookie(session_name(), '', time() - 2592000, '/');
+session_destroy();
+
+// Redirect to home page
+header('Location: index.php');
+
+?>
